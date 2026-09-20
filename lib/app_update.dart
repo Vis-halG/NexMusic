a613fd455cb3c15f9c5eef279d029c530d6e8162
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2026-09-20 23:46:43 +0530

    chore(release): bump app version to 0.2.9+4015 to trigger in-app update popup

--- lib/app_update.dart
+++ lib/app_update.dart
@@ -8,7 +8,7 @@ import 'package:path_provider/path_provider.dart';
 import 'phone_services.dart';
 
 /// Fallback installed version of NexMusic (matches pubspec.yaml).
-const String currentAppVersion = '0.2.9+4014';
+const String currentAppVersion = '0.2.9+4015';
 
 class AppUpdateInfo {
   const AppUpdateInfo({
